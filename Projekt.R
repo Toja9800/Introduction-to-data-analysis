c1733f11cd42c1f038e54794d0ad283065850eb5
K <bulat9800@gmail.com>
2023-06-19 14:03:06 +0200

    -

--- Projekt.R
+++ Projekt.R
@@ -1,3 +1,3 @@
-raw_data <- read.csv("C:/Users/Lenovo/Downloads/archive/heart_failure_clinical_records_dataset.csv", stringsAsFactors = FALSE)
+raw_data <- read.csv("E:/RStudio/Projekt/KNN-R/heart_failure_clinical_records_dataset.csv", stringsAsFactors = FALSE)
 raw_data
 summary(raw_data)
